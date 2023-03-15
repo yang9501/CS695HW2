224880506ee375567661fcf1fef85eedd51f7b37
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-03-15 02:16:04 -0400

    Button push threads working

--- trafficLightsMultiThread.c
+++ trafficLightsMultiThread.c
@@ -122,7 +122,7 @@ static void setLightInitialState(char *greenPort, char *yellowPort, char *redPor
 }
 
 void *cycleLights(void *trafficLightPortsPointer) {
-    char* trafficLightPorts[] = (char*) trafficLightPortsPointer;
+    char* trafficLightPorts[] = (char**) trafficLightPortsPointer;
     #ifdef DEBUG
     (void) printf("Green1 on: %s\n", trafficLightPorts[0]);
     (void) printf("Red1 off: %s\n", trafficLightPorts[2]);
