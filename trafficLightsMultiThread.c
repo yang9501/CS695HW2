26e12f4f9188f3858020bb4f9656a3740bc7945f
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-03-15 00:18:49 -0400

    Thread testing

--- trafficLightsMultiThread.c
+++ trafficLightsMultiThread.c
@@ -57,12 +57,33 @@ int main(void) {
     }
     #endif
 
-    getButtonPressDuration(buttonPorts[0]);
+    //getButtonPressDuration(buttonPorts[0]);
 //   while(1) {
 //		cycleLights(trafficLight1Ports[0], trafficLight1Ports[1], trafficLight1Ports[2], trafficLight2Ports[0], trafficLight2Ports[1], trafficLight2Ports[2]);
 //	}
 
+
+    pthread_t thread1, thread2;
+    char *message1 = "Thread 1";
+    char *message2 = "Thread 2";
+    int iret1, iret2;
+
+    /* Create independent threads each of which will execute function */
+
+    iret1 = pthread_create( &thread1, NULL, print_message_function, (void*) message1);
+    iret2 = pthread_create( &thread2, NULL, print_message_function, (void*) message2);
+    pthread_join( thread1, NULL);
+    pthread_join( thread2, NULL);
+    printf("Thread 1 returns: %d\n",iret1);
+    printf("Thread 2 returns: %d\n",iret2);
+
 	return 0;
+}
+
+void *print_message_function( void *ptr ) {
+    char *message;
+    message = (char *) ptr;
+    printf("%s \n", message);
 }
 
 static void getButtonPressDuration(char *buttonPort) {
