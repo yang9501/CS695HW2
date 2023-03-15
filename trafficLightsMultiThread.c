cfff0148a16a7349310041a760624b2d7b7504dd
Justin Yang <justinyang@Justins-MacBook-Pro.local>
2023-03-14 23:50:01 -0400

    testing

--- trafficLightsMultiThread.c
+++ trafficLightsMultiThread.c
@@ -76,7 +76,7 @@ static void testButton(char *buttonPort1, char *buttonPort2) {
     while(1) {
         ledValue = readLED("/value", buttonPort1);
         if(ledValue == 1){
-            //printf("%d", pressedFlag);
+            printf("%d", pressedFlag);
             //first press detected
             if(pressedFlag == 0) {
                 printf("FIRST PRESSED");
